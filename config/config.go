bd9af262ba2fb042274317b12a8c7a21f7fb1aba
abdurahmon-saydullayev <abdurahmonsaydullayev5@gmail.com>
2023-08-20 17:30:24 +0500

    host ip is changed

--- config/config.go
+++ config/config.go
@@ -36,7 +36,7 @@ type Config struct {
 	DefaultOffset string
 	DefaultLimit  string
 
-	SecretKey  string
+	SecretKey string
 
 	PasscodePool   string
 	PasscodeLength int
@@ -60,7 +60,7 @@ func Load() Config {
 	config.HTTPPort = cast.ToString(getOrReturnDefaultValue("HTTP_PORT", ":9090"))
 	config.HTTPScheme = cast.ToString(getOrReturnDefaultValue("HTTP_SCHEME", "http"))
 
-	config.PostgresHost = cast.ToString(getOrReturnDefaultValue("POSTGRES_HOST", "54.93.209.25"))
+	config.PostgresHost = cast.ToString(getOrReturnDefaultValue("POSTGRES_HOST", "3.72.3.59"))
 	config.PostgresPort = cast.ToInt(getOrReturnDefaultValue("POSTGRES_PORT", 5432))
 	config.PostgresUser = cast.ToString(getOrReturnDefaultValue("POSTGRES_USER", "abdurahmon"))
 	config.PostgresPassword = cast.ToString(getOrReturnDefaultValue("POSTGRES_PASSWORD", "aus1003"))
